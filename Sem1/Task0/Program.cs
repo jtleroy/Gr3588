21952005d2d9c37670463fc8cbc5642cdf913a53
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-04 16:28:27 +0300

    Added task0

---
+++ Sem1/Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+// Прверяем, чтобы данные были не пустями
+if(inputLine!=null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим квадрат числа
+    //int outNumber = inputNumber*inputNumber;
+    //int outNumber = (int)Math.Pow(inputNumber,2);
+    
+    // Выводим значение в консоль
+    //Console.WriteLine(outNumber);
+    Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+}
