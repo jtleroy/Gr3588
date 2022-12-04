4937bf6e9cc5f767bd3ee2f87e3fdadddfe9c2ef
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-04 18:48:23 +0300

    Added task4

---
+++ Sem1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 7. Напишите программу, которая принимает на вход трехзначное число и нвыходе показывает последнюю цифру этого числа.
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine!=null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int lastDigit = inputNumber % 10;
+
+    Console.WriteLine(lastDigit);
+}
+
