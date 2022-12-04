1e86b8d449f7cc6e217e3d22c271ad8c882d6a12
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-04 18:24:40 +0300

    Added task3

---
+++ Sem1/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+//Считываем данные с консоли
+string? inputeLine = Console.ReadLine();
+
+// Проверяем, чтобы данные были не пустыми
+if(inputeLine!=null)
+{
+    //Парсим введенное число
+    int inputeNumber = int.Parse(inputeLine);
+    int starNumber = inputeNumber*(-1);
+
+    // Выходное значение
+    string outLine = string.Empty;
+
+    while(starNumber<inputeNumber)
+    {
+        outLine = outLine + starNumber + ",";
+        starNumber++;
+        //starNumber=starNumber+1
+
+    }
+    outLine = outLine + inputeNumber;
+    // Выводим данные в консоль
+    Console.WriteLine(outLine);
+}
