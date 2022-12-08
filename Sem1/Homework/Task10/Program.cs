476ffec1c66be27da86a857a6d9dce7cb915892f
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-08 19:25:50 +0300

    Added homework task10

---
+++ Sem1/Homework/Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+
+Console.WriteLine("Введите трехзначное число");
+string? numberline = Console.ReadLine();
+
+if (numberline != null)
+{
+    int number = int.Parse(numberline);
+    number = number % 100;
+    number = number / 10;
+    Console.WriteLine(number);
+}
