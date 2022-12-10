ef0212822d98a321b654cffe3a5a145fc602c8bd
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-10 14:52:21 +0300

    Added a second solution task13

--- Sem1/Homework/Task13/Program.cs
+++ Sem1/Homework/Task13/Program.cs
@@ -3,28 +3,60 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-//метод, который считывает int с консоли
-static int ReadNumber(string text) 
-{
-    Console.WriteLine(text);
-    string? numberline = Console.ReadLine();
-    if (numberline != null)
-    {
-        int number = int.Parse(numberline);
-        return number;
+// //мой вариант решения задачи
+// //метод, который считывает int с консоли
+// static int ReadNumber(string text) 
+// {
+//     Console.WriteLine(text);
+//     string? numberline = Console.ReadLine();
+//     if (numberline != null)
+//     {
+//         int number = int.Parse(numberline);
+//         return number;
+//     }
+//     return 0;
+// }
+
+// int number = ReadNumber("Введите число");
+
+// if (number > 99) 
+// {
+//     string arrayOfDigits = number.ToString(); //преобразуем int в string, чтобы пользоваться строкой как массивом
+//     Console.WriteLine(arrayOfDigits[2]); //выводим данные в консоль
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет"); //выводим данные в консоль
+// }
+
+//вариант решения 2
+
+//вводим число
+Console.WriteLine("Введите число"); 
+int number = int.Parse(Console.ReadLine()??"0");
+
+// Делаем цикл, если в числе минимум три цифры
+if(number>99)
+{   
+    //Находим количество цифр в цисле (или можно сделать циклом while, делить number/10, пока number>10, счетчик в цикле++) 
+    int length = (int)Math.Log10(number) + 1; 
+
+    int index = 0;
+    int thirdDigit = number;
+
+    // делаем деление числа на 10 (lingth-3) раз, чтобы осталось три цифры в числе
+    while(index < length-3)
+    {   
+        thirdDigit = thirdDigit/10;
+        index++;
     }
-    return 0;
+
+    // находим третью цифру остатком от деления на 10
+    thirdDigit = thirdDigit % 10;
+    // находим тертье число
+    Console.Write(thirdDigit + " " + "Является третьей цифрой числа " + number);
 }
-
-int number = ReadNumber("Введите число");
-
-if (number > 99) 
+else 
 {
-    string arrayOfDigits = number.ToString(); //преобразуем int в string, чтобы пользоваться строкой как массивом
-    Console.WriteLine(arrayOfDigits[2]); //выводим данные в консоль
+    Console.Write("третьей цифры нет");
 }
-else
-{
-    Console.WriteLine("Третьей цифры нет"); //выводим данные в консоль
-}
-
