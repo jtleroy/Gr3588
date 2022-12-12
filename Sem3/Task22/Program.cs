4273273131adbfe013c0eaccf25ad3f943d6d8c2
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-12 22:17:01 +0300

    Update task22

---
+++ Sem3/Task22/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов от 1 до N
+
+
+// метод читает данные пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()?? "0");
+}
+
+// Метод сборки строки
+string LineBuilder(int n, int pow)
+{
+    string line = string.Empty;
+    for (int i=1; i<n; i++)
+    {
+        //line = line + Math.Pow(i, pow) + ",";
+        line = $"{line}{Math.Pow(i, pow)} ";
+    }
+    line = line + Math.Pow(n, pow);
+    return line;
+};
+
+// Метод вывода данных
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+
+int number = ReadData("Введите число: ");
+
+string outLine = LineBuilder(number, 1);
+PrintData(" ", outLine);
+
+outLine = LineBuilder(number, 2 );
+PrintData(" ", outLine);
+
+
+
+
+
+
+
